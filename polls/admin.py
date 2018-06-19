d7d09b220c7d9cc1739055afd1f604f1c2cdcd95
Tony Cook <cook.anthony@gmail.com>
2018-06-19 14:27:17 -0700

    Tutorial part 7: customizing admin pages

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
 #admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     #fields = ['pub_date', 'question_text']
@@ -10,5 +14,6 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,                  {'fields': ['question_text']}),
         ('Date information',    {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
